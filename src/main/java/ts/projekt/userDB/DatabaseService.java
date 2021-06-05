1fb695851eb860dff80fb2ce6380885925448dec
sabre <djsoundnr1@gmail.com>
2021-06-05 18:20:16 +0200

    Method for registering users

--- src/main/java/ts/projekt/userDB/DatabaseService.java
+++ src/main/java/ts/projekt/userDB/DatabaseService.java
@@ -1,9 +1,6 @@
 package ts.projekt.userDB;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 
@@ -28,5 +25,13 @@ public class DatabaseService {
             throwables.printStackTrace();
         }
         return user;
+    }
+    @PostMapping(path = "register")
+    public User register(@RequestBody User user) throws SQLException {
+        int id = Database.database.addUser(user);
+        if (id == 0) {
+            return null;
+        }
+        else return Database.database.getUser(new User(id));
     }
 }
